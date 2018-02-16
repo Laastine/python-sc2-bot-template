3152593ccc8938a470c79f22111d15b8c19d3850
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-02-16 09:22:21 +0200

    Reindent python files with two spaces

--- bot/main.py
+++ bot/main.py
@@ -4,9 +4,9 @@ from pathlib import Path
 import sc2
 
 class MyBot(sc2.BotAI):
-    with open(Path(__file__).parent / "../botinfo.json") as f:
-        NAME = json.load(f)["name"]
+  with open(Path(__file__).parent / "../botinfo.json") as f:
+    NAME = json.load(f)["name"]
 
-    async def on_step(self, iteration):
-        if iteration == 0:
-            await self.chat_send(f"Name: {self.NAME}")
+  async def on_step(self, iteration):
+    if iteration == 0:
+      await self.chat_send(f"Name: {self.NAME}")
--- run_locally.py
+++ run_locally.py
@@ -6,15 +6,15 @@ from sc2.player import Bot, Computer
 from bot import MyBot
 
 def main():
-    with open("botinfo.json") as f:
-        info = json.load(f)
+  with open("botinfo.json") as f:
+    info = json.load(f)
 
-    race = Race[info["race"]]
+  race = Race[info["race"]]
 
-    run_game(maps.get("Abyssal Reef LE"), [
-        Bot(race, MyBot()),
-        Computer(Race.Random, Difficulty.Medium)
-    ], realtime=False, step_time_limit=2.0, game_time_limit=(60*20), save_replay_as="test.SC2Replay")
+  run_game(maps.get("Abyssal Reef LE"), [
+    Bot(race, MyBot()),
+    Computer(Race.Random, Difficulty.Medium)
+  ], realtime=False, step_time_limit=2.0, game_time_limit=(60 * 20), save_replay_as="test.SC2Replay")
 
 if __name__ == '__main__':
-    main()
+  main()
