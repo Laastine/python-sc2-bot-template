8e621bafa40cce6bdd703eb72f3fc62d45a7bca4
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2018-02-16 11:17:41 +0200

    Oops.

--- bot/main.py
+++ bot/main.py
@@ -61,14 +61,14 @@ class MyBot(sc2.BotAI):
       for SCVs in self.workers.random_group_of(3):
         await self.do(SCVs.gather(refinery))
 
-    if self.known_enemy_units.amount > 0:
+    if self.known_enemy_units.amount > 0 and iteration % 15 == 0:
       print(f'Found enemy units! {self.known_enemy_units}')
       for unit in self.marines_excluding_scout():
         await self.do(unit.attack(self.known_enemy_units[0]))
-    elif self.units(MARINE).amount > 14:
+    elif self.units(MARINE).amount > 14 and iteration % 15 == 0:
       print(f'Rush time!')
       for unit in self.marines_excluding_scout():
-        print(f'Go go go gooo!')
+        print(f'Go go go gooo! {unit}')
         await self.do(unit.attack(self.enemy_start_locations[0]))
 
     # Barracks
@@ -96,7 +96,7 @@ class MyBot(sc2.BotAI):
     await self.scout(iteration)
 
   def marines_excluding_scout(self):
-    return (x for x in self.units(MARINE) if x.tag == self.scout_tag)
+    return (x for x in self.units(MARINE) if x.tag != self.scout_tag)
 
   def find_marine_by_tag(self, unit_tag):
     for marine in self.units(MARINE):
