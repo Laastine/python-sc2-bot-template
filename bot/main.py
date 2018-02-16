7f923b9c15bc76fa5481789ede7ca095cd4881dd
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-02-16 09:52:12 +0200

    Build supply depots and max 3 raxes

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,9 @@ import json
 from pathlib import Path
 
 import sc2
+from sc2.constants import *
+from sc2 import Race, Difficulty
+from sc2.player import Bot, Computer
 
 class MyBot(sc2.BotAI):
   with open(Path(__file__).parent / "../botinfo.json") as f:
@@ -10,3 +13,26 @@ class MyBot(sc2.BotAI):
   async def on_step(self, iteration):
     if iteration == 0:
       await self.chat_send(f"Name: {self.NAME}")
+
+    cc = self.units(COMMANDCENTER)
+    if not cc.exists:
+      target = self.known_enemy_structures.random_or(self.enemy_start_locations[0]).position
+      for unit in self.workers | self.units(MARINE):
+        await self.do(unit.attack(target))
+      return
+    else:
+      cc = cc.first
+
+    for scv in self.units(SCV).idle:
+      await self.do(scv.gather(self.state.mineral_field.closest_to(cc)))
+
+    if self.can_afford(SCV) and self.workers.amount < 16 and cc.noqueue:
+      await self.do(cc.train(SCV))
+
+    elif self.supply_left < (2 if self.units(BARRACKS).amount < 3 else 4):
+      if self.can_afford(SUPPLYDEPOT):
+        await self.build(SUPPLYDEPOT, near=cc.position.towards(self.game_info.map_center, 3))
+
+    elif self.units(BARRACKS).amount < 3 or (self.minerals > 400 and self.units(BARRACKS).amount < 4):
+      if self.can_afford(BARRACKS):
+        await self.build(BARRACKS, near=cc.position.towards(self.game_info.map_center, 7))
