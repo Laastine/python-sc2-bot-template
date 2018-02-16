c54bb7a0dec5c6699170373303cdab5e0be18bb3
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-02-16 10:31:26 +0200

    Build gas extractor

--- bot/main.py
+++ bot/main.py
@@ -11,6 +11,10 @@ class MyBot(sc2.BotAI):
   def __init__(self):
     super()
     self.scout_index = 0
+    self.scv_counter = 0
+    self.extractor_started = False
+    self.moved_workers_to_gas = False
+    self.moved_workers_from_gas = False
 
   with open(Path(__file__).parent / "../botinfo.json") as f:
     NAME = json.load(f)["name"]
@@ -39,6 +43,21 @@ class MyBot(sc2.BotAI):
     elif self.supply_left < (2 if self.units(BARRACKS).amount < 3 else 4):
       if self.can_afford(SUPPLYDEPOT):
         await self.build(SUPPLYDEPOT, near=cc.position.towards(self.game_info.map_center, 3))
+
+    # Gas
+    if not self.extractor_started:
+      if self.can_afford(REFINERY):
+        SCVs = self.workers.random
+        target = self.state.vespene_geyser.closest_to(SCVs.position)
+        err = await self.do(SCVs.build(REFINERY, target))
+        if not err:
+          self.extractor_started = True
+
+    if self.units(REFINERY).ready.exists and not self.moved_workers_to_gas:
+      self.moved_workers_to_gas = True
+      refinery = self.units(REFINERY).first
+      for SCVs in self.workers.random_group_of(3):
+        await self.do(SCVs.gather(refinery))
 
     # Barracks
     elif self.units(BARRACKS).amount < 3 or (self.minerals > 400 and self.units(BARRACKS).amount < 4):
